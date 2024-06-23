8958026a55f24997aefad2b7d9be19998eae52d3
mudegiarnold <136020298+mudegiarnold@users.noreply.github.com>
2024-06-23 19:09:22 +0000

    added files

---
+++ main.go
@@ -0,0 +1,13 @@
+package main 
+import (
+	"fmt"
+	"log"
+	
+	"github.com/gofiber/fiber/v2"
+)
+func main() {
+	fmt.Println("Hello world") 
+	app := fiber.New()
+	
+	log.Fatal(app.Listen(":4040"))
+	}
